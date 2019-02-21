5d496309aab9863086cc89dfd992b411d6ee1ee7
magic <226223359@qq.com>
2019-02-21 17:56:11 +0800

    初始化

--- app/src/main/java/com/guodongbaohe/app/activity/AboutUsActivity.java
+++ app/src/main/java/com/guodongbaohe/app/activity/AboutUsActivity.java
@@ -20,7 +20,7 @@ import com.guodongbaohe.app.util.WebViewUtil;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/*1227平均线盘*/
+/*1227均*/
 public class AboutUsActivity extends BaseActivity {
     @BindView(R.id.webview)
     WebView webview;

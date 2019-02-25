2828de9cc898c052ee1d9868638f03de5d34d132
gaoman <qwe1234>
2019-02-25 13:52:28 +0800

    1。压缩图片

--- app/src/main/java/com/guodongbaohe/app/util/QRCodeUtil.java
+++ app/src/main/java/com/guodongbaohe/app/util/QRCodeUtil.java
@@ -1,6 +1,7 @@
 package com.guodongbaohe.app.util;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
@@ -13,6 +14,8 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.Hashtable;
 
 /**
@@ -76,8 +79,20 @@ public class QRCodeUtil {
     public static Bitmap createQRCodeBitmap(String content, int size, Bitmap targetBitmap) {
         return createQRCodeBitmap(content, size, "UTF-8", "H", "4", Color.BLACK, Color.WHITE, targetBitmap, null, 0F);
     }
+    private static Bitmap compressImage(Bitmap image) {
 
-    /**
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 100;
+        while ( baos.toByteArray().length / 1024>100) { //循环判断如果压缩后图片是否大于100kb,大于继续压缩
+            baos.reset();//重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+            options -= 10;//每次都减少10
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+        return bitmap;
+    }    /**
      * 创建二维码位图 (支持自定义配置和自定义样式)
      *
      * @param content          字符串内容
@@ -152,7 +167,7 @@ public class QRCodeUtil {
                 return addLogo(bitmap, logoBitmap, logoPercent);
             }
 
-            return bitmap;
+            return compressImage(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
         }

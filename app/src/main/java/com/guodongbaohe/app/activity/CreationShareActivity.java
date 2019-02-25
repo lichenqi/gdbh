6911667cfc699b92cd8440a8c93ac80f264e695e
magic <226223359@qq.com>
2019-02-25 14:03:49 +0800

    初始化

--- app/src/main/java/com/guodongbaohe/app/activity/CreationShareActivity.java
+++ app/src/main/java/com/guodongbaohe/app/activity/CreationShareActivity.java
@@ -24,6 +24,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;

5e56e3e93bdbbec1422efef09a1e18e824a027ff
gaoman <qwe1234>
2019-03-13 10:14:10 +0800

    1。列表点击跳转

--- app/src/main/java/com/guodongbaohe/app/bean/ScrollBean.java
+++ app/src/main/java/com/guodongbaohe/app/bean/ScrollBean.java
@@ -19,10 +19,14 @@ public class ScrollBean extends SectionEntity<ScrollBean.ScrollItemBean> {
     public static class ScrollItemBean {
         private String text;
         private String type;
+        private String id;
+        private String p_id;
 
-        public ScrollItemBean(String text, String type) {
+        public ScrollItemBean(String text, String type,String id,String p_id) {
             this.text = text;
             this.type = type;
+            this.id=id;
+            this.p_id=p_id;
         }
 
         public String getText() {
@@ -39,6 +43,22 @@ public class ScrollBean extends SectionEntity<ScrollBean.ScrollItemBean> {
 
         public void setType(String type) {
             this.type = type;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getP_id() {
+            return p_id;
+        }
+
+        public void setP_id(String p_id) {
+            this.p_id = p_id;
         }
     }
 }

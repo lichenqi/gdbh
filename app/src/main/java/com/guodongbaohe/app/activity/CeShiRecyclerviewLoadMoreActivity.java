efc6fbcc1eea461c14c355783ac1792447076b82
magic <226223359@qq.com>
2019-05-28 10:39:38 +0800

    初始化

--- app/src/main/java/com/guodongbaohe/app/activity/CeShiRecyclerviewLoadMoreActivity.java
+++ app/src/main/java/com/guodongbaohe/app/activity/CeShiRecyclerviewLoadMoreActivity.java
@@ -56,96 +56,96 @@ public class CeShiRecyclerviewLoadMoreActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ButterKnife.bind(this);
-        setMiddleTitle("加载更多");
-        recyclerview.setHasFixedSize(true);
-        recyclerview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        super.onCreate( savedInstanceState );
+        ButterKnife.bind( this );
+        setMiddleTitle( "加载更多" );
+        recyclerview.setHasFixedSize( true );
+        recyclerview.setLayoutManager( new LinearLayoutManager( getApplicationContext() ) );
         getListData();
         initRecyclerData();
-        swiperefreshlayout.setColorSchemeColors(Color.parseColor("#EFA818"));
-        swiperefreshlayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swiperefreshlayout.setColorSchemeColors( Color.parseColor( "#EFA818" ) );
+        swiperefreshlayout.setOnRefreshListener( new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                new Handler().postDelayed(new Runnable() {
+                new Handler().postDelayed( new Runnable() {
                     public void run() {
                         pageNum = 1;
                         data_is_exist = 1;
-                        adapter.changeState(data_is_exist);
-                        swiperefreshlayout.setRefreshing(false);
+                        adapter.changeState( data_is_exist );
+                        swiperefreshlayout.setRefreshing( false );
                     }
-                }, 1500);
+                }, 1500 );
             }
-        });
+        } );
     }
 
     private void initRecyclerData() {
-        adapter = new LoadMoreAdapter(list);
-        recyclerview.setAdapter(adapter);
-        recyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        adapter = new LoadMoreAdapter( list );
+        recyclerview.setAdapter( adapter );
+        recyclerview.addOnScrollListener( new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
+                super.onScrollStateChanged( recyclerView, newState );
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
                     //到达底部之后如果footView的状态不是正在加载的状态,就将 他切换成正在加载的状态
-                    new Handler().postDelayed(new Runnable() {
+                    new Handler().postDelayed( new Runnable() {
                         @Override
                         public void run() {
                             pageNum++;
                             if (data_is_exist == 1) {
                                 getListData();
                             }
-                            adapter.changeState(data_is_exist);
+                            adapter.changeState( data_is_exist );
                         }
-                    }, 1500);
+                    }, 1500 );
                 }
             }
 
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
+                super.onScrolled( recyclerView, dx, dy );
             }
-        });
+        } );
     }
 
     /*列表数据*/
     private void getListData() {
         LinkedHashMap<String, String> map = new LinkedHashMap<>();
-        map.put("limit", "12");
-        map.put("supid", "154");
-        map.put("page", String.valueOf(pageNum));
-        String param = ParamUtil.getMapParam(map);
-        Log.i("列表数据参数", Constant.BASE_URL + Constant.SHOP_LIST + "?" + param);
+        map.put( "limit", "12" );
+        map.put( "supid", "154" );
+        map.put( "page", String.valueOf( pageNum ) );
+        String param = ParamUtil.getMapParam( map );
+        Log.i( "列表数据参数", Constant.BASE_URL + Constant.SHOP_LIST + "?" + param );
         MyApplication.getInstance().getMyOkHttp().post()
-                .url(Constant.BASE_URL + Constant.SHOP_LIST + "?" + param)
-                .tag(this)
-                .addHeader("x-appid", Constant.APPID)
-                .addHeader("x-devid", PreferUtils.getString(getApplicationContext(), Constant.PESUDOUNIQUEID))
-                .addHeader("x-nettype", PreferUtils.getString(getApplicationContext(), Constant.NETWORKTYPE))
-                .addHeader("x-agent", VersionUtil.getVersionCode(getApplicationContext()))
-                .addHeader("x-platform", Constant.ANDROID)
-                .addHeader("x-devtype", Constant.IMEI)
-                .addHeader("x-token", ParamUtil.GroupMap(getApplicationContext(), ""))
-                .enqueue(new JsonResponseHandler() {
+                .url( Constant.BASE_URL + Constant.SHOP_LIST + "?" + param )
+                .tag( this )
+                .addHeader( "x-appid", Constant.APPID )
+                .addHeader( "x-devid", PreferUtils.getString( getApplicationContext(), Constant.PESUDOUNIQUEID ) )
+                .addHeader( "x-nettype", PreferUtils.getString( getApplicationContext(), Constant.NETWORKTYPE ) )
+                .addHeader( "x-agent", VersionUtil.getVersionCode( getApplicationContext() ) )
+                .addHeader( "x-platform", Constant.ANDROID )
+                .addHeader( "x-devtype", Constant.IMEI )
+                .addHeader( "x-token", ParamUtil.GroupMap( getApplicationContext(), "" ) )
+                .enqueue( new JsonResponseHandler() {
 
                     @Override
                     public void onSuccess(int statusCode, JSONObject response) {
-                        super.onSuccess(statusCode, response);
-                        Log.i("列表数据", response.toString());
+                        super.onSuccess( statusCode, response );
+                        Log.i( "列表数据", response.toString() );
                         try {
-                            JSONObject jsonObject = new JSONObject(response.toString());
-                            if (jsonObject.getInt("status") >= 0) {
-                                HomeListBean bean = GsonUtil.GsonToBean(response.toString(), HomeListBean.class);
+                            JSONObject jsonObject = new JSONObject( response.toString() );
+                            if (jsonObject.getInt( "status" ) >= 0) {
+                                HomeListBean bean = GsonUtil.GsonToBean( response.toString(), HomeListBean.class );
                                 if (bean == null) return;
                                 listData = bean.getResult();
                                 if (listData.size() > 1) {
                                     data_is_exist = 1;
                                     if (pageNum == 1) {
                                         list.clear();
-                                        list.addAll(listData);
+                                        list.addAll( listData );
                                         adapter.notifyDataSetChanged();
                                     } else {
-                                        list.addAll(listData);
+                                        list.addAll( listData );
                                         adapter.notifyDataSetChanged();
                                     }
                                 } else {
@@ -163,7 +163,7 @@ public class CeShiRecyclerviewLoadMoreActivity extends BaseActivity {
                     public void onFailure(int statusCode, String error_msg) {
                         data_is_exist = -1;
                     }
-                });
+                } );
     }
 
 
@@ -187,11 +187,11 @@ public class CeShiRecyclerviewLoadMoreActivity extends BaseActivity {
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view;
             if (viewType == item_normal) {
-                view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.load_more_item, parent, false);
-                return new LoadMoreHolder(view);
+                view = LayoutInflater.from( getApplicationContext() ).inflate( R.layout.load_more_item, parent, false );
+                return new LoadMoreHolder( view );
             } else if (viewType == item_foot) {
-                view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.load_more_foot_view, parent, false);
-                return new FootHolder(view);
+                view = LayoutInflater.from( getApplicationContext() ).inflate( R.layout.load_more_foot_view, parent, false );
+                return new FootHolder( view );
             }
             return null;
         }
@@ -199,16 +199,16 @@ public class CeShiRecyclerviewLoadMoreActivity extends BaseActivity {
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
             if (holder instanceof LoadMoreHolder) {
-                Glide.with(getApplicationContext()).load(list.get(position).getGoods_thumb()).into(((LoadMoreHolder) holder).iv);
+                Glide.with( getApplicationContext() ).load( list.get( position ).getGoods_thumb() ).into( ((LoadMoreHolder) holder).iv );
             } else if (holder instanceof FootHolder) {
                 switch (state) {
                     case 1:/*还有数据*/
-                        ((FootHolder) holder).progressBar.setVisibility(View.VISIBLE);
-                        ((FootHolder) holder).tv_foot.setVisibility(View.GONE);
+                        ((FootHolder) holder).progressBar.setVisibility( View.VISIBLE );
+                        ((FootHolder) holder).tv_foot.setVisibility( View.GONE );
                         break;
                     case -1:/*没有更多数据了*/
-                        ((FootHolder) holder).progressBar.setVisibility(View.GONE);
-                        ((FootHolder) holder).tv_foot.setVisibility(View.VISIBLE);
+                        ((FootHolder) holder).progressBar.setVisibility( View.GONE );
+                        ((FootHolder) holder).tv_foot.setVisibility( View.VISIBLE );
                         break;
                 }
             }
@@ -235,8 +235,8 @@ public class CeShiRecyclerviewLoadMoreActivity extends BaseActivity {
         ImageView iv;
 
         public LoadMoreHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
+            super( itemView );
+            ButterKnife.bind( this, itemView );
         }
     }
 
@@ -247,8 +247,8 @@ public class CeShiRecyclerviewLoadMoreActivity extends BaseActivity {
         TextView tv_foot;
 
         public FootHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
+            super( itemView );
+            ButterKnife.bind( this, itemView );
         }
     }
 
